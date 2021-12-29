c2ed7b4a61d01d457827df51588dbed5de6c7055
Vanja Kostanjevac <vkostanjevac@omega-software.hr>
2021-12-29 16:15:41 +0100

    Day 4

---
+++ Bookstore/Bookstore.Concepts/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Rhetos.Dsl;
+using Rhetos.Dsl.DefaultConcepts;
+
+
+
+namespace Rhetos.Dsl.DefaultConcepts
+{
+    /// <summary>
+    /// Automatically enters time when the records was created.
+    /// </summary>
+    [Export(typeof(IConceptInfo))]
+    [ConceptKeyword("LastModifiedTime")]
+    public class LastModifiedTimeInfo : IConceptInfo
+    {
+        [ConceptKey]
+        public DateTimePropertyInfo Property { get; set; }
+    }
+}
+
---
+++ Bookstore/Bookstore.Concepts/Interface1.cs
@@ -0,0 +1,32 @@
+﻿using Rhetos.Compiler;
+using Rhetos.Dsl.DefaultConcepts;
+using Rhetos.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+
+namespace Rhetos.Dom.DefaultConcepts
+{
+    [Export(typeof(IConceptCodeGenerator))]
+    [ExportMetadata(MefProvider.Implements, typeof(LastModifiedTimeInfo))]
+    public class LastModifiedTimeCodeGenerator : IConceptCodeGenerator
+    {
+        public void GenerateCode(Dsl.IConceptInfo conceptInfo, ICodeBuilder codeBuilder)
+        {
+            var info = (LastModifiedTimeInfo)conceptInfo;
+
+            string snippet =
+            $@"{{ 
+                var now = SqlUtility.GetDatabaseTime(_executionContext.SqlExecuter);
+                foreach (var newItem in insertedNew)
+                    if(newItem.{info.Property.Name} == null)
+                        newItem.{info.Property.Name} = now;
+            }}
+            ";
+
+            codeBuilder.InsertCode(snippet, WritableOrmDataStructureCodeGenerator.InitializationTag, info.Property.DataStructure);
+        }
+    }
+}

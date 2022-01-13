adef8072865e43266747cc7e7a794b9ba9430a72
Vanja Kostanjevac <vkostanjevac@omega-software.hr>
2022-01-13 08:53:02 +0100

    Data migration day 6

---
+++ Bookstore/Bookstore_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bookstore_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Omega software d.o.o.")]
+[assembly: AssemblyProduct("Bookstore_Test")]
+[assembly: AssemblyCopyright("Copyright © Omega software d.o.o. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79c4d255-e1ef-4c33-9513-1d1b509c4452")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
